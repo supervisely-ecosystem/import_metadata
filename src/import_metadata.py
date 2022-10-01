2a6918eadf42fac08f073149f33d04db9828f4fb
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 20:16:23 +0000

    Adding tarfile member sanitization to extractall()

--- src/import_metadata.py
+++ src/import_metadata.py
@@ -56,7 +56,26 @@ def import_metadata(api: sly.Api, task_id, context, state, app_logger):
         with tarfile.open(archive_path) as archive:
              extract_dir = os.path.join(storage_dir, INPUT_PATH.strip('/'))
              extract_dir = os.path.abspath(os.path.join(os.path.dirname(extract_dir), '.'))
-             archive.extractall(extract_dir)
+             def is_within_directory(directory, target):
+                 
+                 abs_directory = os.path.abspath(directory)
+                 abs_target = os.path.abspath(target)
+             
+                 prefix = os.path.commonprefix([abs_directory, abs_target])
+                 
+                 return prefix == abs_directory
+             
+             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+             
+                 for member in tar.getmembers():
+                     member_path = os.path.join(path, member.name)
+                     if not is_within_directory(path, member_path):
+                         raise Exception("Attempted Path Traversal in Tar File")
+             
+                 tar.extractall(path, members, numeric_owner) 
+                 
+             
+             safe_extract(archive, extract_dir)
     else:
         raise ValueError("No such file {}".format(INPUT_PATH))
 
